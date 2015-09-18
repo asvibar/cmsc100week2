23d1127660b6447d717264428f1254e1d408bcea
alyssachelseah <mnblaquera@up.edu.ph>
2015-09-18 10:46:55 +0800

    more tests

--- test/student.js
+++ test/student.js
@@ -34,11 +34,14 @@ var request = require('supertest');
          it('Should return a newly created student record',function(done){
             request(url)
             .post('/students')
-            .send({'studNo':'2069-67891','name':'Kek Master','bdate':'2000-12-12'})
+            .send({'studNo':'2069-67899','name':'Kek Master','bdate':'2000-12-12'})
             .end(function(err,res){
                if(err) throw err;
                res.should.have.status(200); //test for response
                res.body.should.instanceof(Object); //test body of response
+               res.should.have.property('studNo','name','bdate');
+               res.studNo.should.match(/^\d{4}-\d{5}$/);
+          //     res.bdate.should.match(/^\d{4}-(01|02|03|04|05|06|07|08|09|10|11|12)-(01|02|03|04|05|06|07|08|09|10|11|2|13|14|15|16|17|19|20|21|22|23|24|25|26|27|28|29|30|31)$/);
                
                done();
             });//error handling and test
